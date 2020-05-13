6ec646355495a96924e60f813c2ff9686e6215ac
jkyle109 <jkyle109@gmail.com>
2020-05-13 04:42:11 -0400

    Added API links and fetch functions.

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const token = auth.token;
 const site = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/CjVoLPfQ22SjJNcnwJfo0Ucx0TX4-5-vjPENNxIXkEPiOVM?api_key=" + auth.key
 const prefix = "!";
 
+// Constants
+// const api = `?api_key=${auth.key}`;
+// const protocol = "https://";
+// const regions = {
+//   BR1: "br1.api.riotgames.com",
+//   EUN1: "eun1.api.riotgames.com",
+//   EUW1: "euw1.api.riotgames.com",
+//   JP1: "jp1.api.riotgames.com",
+//   KR: "kr.api.riotgames.com",
+//   LA1: "la1.api.riotgames.com",
+//   LA2: "la2.api.riotgames.com",
+//   NA1: "na1.api.riotgames.com",
+//   OC1: "oc1.api.riotgames.com",
+//   TR1: "tr1.api.riotgames.com",
+//   RU: "ru.api.riotgames.com"
+// }
+
+
 bot.on('ready', () =>{
   console.log("This bot is online!");
 });
@@ -44,6 +62,65 @@ bot.on("message", async msg=>{
 
 });
 
+// One Time Fetch 
+// let onLoadFetch = async () => {
+//   let result = {};
+//   result.queueIds = await fetch("http://static.developer.riotgames.com/docs/lol/queues.json");
+//   result.seasons = await fetch("http://static.developer.riotgames.com/docs/lol/seasons.json");
+//   result.maps = await fetch("http://static.developer.riotgames.com/docs/lol/maps.json");
+//   result.gameModes = await fetch("http://static.developer.riotgames.com/docs/lol/gameModes.json");
+//   result.gameTypes = await fetch("http://static.developer.riotgames.com/docs/lol/gameTypes.json");
+//   result.champions = await fetch("http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/champion.json");
+//   result.profileIcons = await fetch("http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/profileicon.json");
+//   return result;
+// }
+
+
+// let getData = async () => {
+//   // Test Case
+//   // let summonerName = "Kyle";
+//   // let region = "NA1";
+
+//   // if(region in regions){
+//   //     region = regions[region]
+//   // } else {
+//   //     console.log("error")
+//   //     return;
+//   // }
+
+//   let result = {};
+
+//   // Get summoner data
+//   result.summonerData = await fetch(`${protocol}${region}/lol/summoner/v4/summoners/by-name/${summonerName}${api}`)
+//     .then(response => response.json())
+//     .then(result => result);
+
+//   console.log(result.summonerData)
+//   result.encryptedSummonerId = result.summonerData.id;
+//   result.encryptedAccountId = result.summonerData.accountId;
+
+//   // Use summoner data to query for more data
+//   let query = {
+//     matchHistory: `${protocol}${region}/lol/match/v4/matchlists/by-account/${encryptedAccountId}${api}`,
+//     leagueData: `${protocol}${region}/lol/league/v4/entries/by-summoner/${encryptedSummonerId}${api}`,
+//     championMastery: `${protocol}${region}/lol/champion-mastery/v4/champion-masteries/by-summoner/${encryptedSummonerId}${api}`,
+//   }
+
+//   result.matchHistory = await fetch(query.matchHistory)
+//     .then(response => response.json());
+
+//   result.leagueData = await fetch(query.leagueData)
+//     .then(response => response.json())
+//     .then(result =>  result[0]);
+
+//   result.championMastery = await fetch(query.championMastery)
+//     .then(response => response.json())
+//     .then(result => result.slice(0,5));
+
+//   return result
+// }
+
+
 // getes data from API, returns it into usable json
 async function getData() {
   const response = await fetch(site);
@@ -65,7 +142,7 @@ function getEmbed(info){
       { name: 'LP', value: info[0].leaguePoints, inline: true},
       { name: "W/L", value: info[0].wins + " W / " + info[0].losses + " L", inline: true},
       { name: '\u200B', value: '\u200B' },
-     )
+    )
     .setFooter("Jon and Ric")
     .setTimestamp()
 
